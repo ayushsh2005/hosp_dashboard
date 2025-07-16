1357853a55c0890c2619906ee2b035ebe8e23aef
Ayush Sharma <ayushsharma36498@gmail.com>
2025-07-16 23:33:27 +0530

    dashboard all urls changed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://hosp-backend.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
@@ -34,7 +34,7 @@ const App = () => {
       }
     };
     fetchUser();
-  }, [isAuthenticated]);
+  }, []);
 
   return (
     <Router>
